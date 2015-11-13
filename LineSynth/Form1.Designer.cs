275d59b3e21118bac530046c2a29c581e9f41147
satoshi246ss <satoshi246ss@yahoo.co.jp>
2015-11-14 02:59:35 +0900

    ラベル

--- LineSynth/Form1.Designer.cs
+++ LineSynth/Form1.Designer.cs
@@ -28,34 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(2, 2);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(758, 701);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1234, 701);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1242, 680);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(2, 709);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1234, 65);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1004, 772);
+            this.ClientSize = new System.Drawing.Size(1416, 772);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -67,6 +89,8 @@
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- LineSynth/Form1.cs
+++ LineSynth/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,91 @@ namespace LineSynth
 {
     public partial class Form1 : Form
     {
+        PointF[] obs_data = new PointF[2000] ;
+        int obs_data_max = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void ReadData()
+        {
+            // ファイルからテキストを読み出し。
+            using (StreamReader r = new StreamReader(@"20141122_0459_3_53pw.txt"))
+            {
+                int i=0;
+                string line;
+                while ((line = r.ReadLine()) != null) // 1行ずつ読み出し。
+                {
+                    // カンマ区切りで分割して配列に格納する
+                    string[] stArrayData = line.Split(',');
+                    obs_data[i].X = float.Parse(stArrayData[0]);
+                    obs_data[i].Y = float.Parse(stArrayData[1]);
+                    i++;
+
+                    richTextBox1.AppendText(stArrayData[0]+stArrayData[1]);
+                }
+                obs_data_max = i;
+            }
+             
+            // ファイルからテキストを読み出し。
+            using (StreamReader r = new StreamReader(@"OI.txt"))
+            {
+                string line;
+                while ((line = r.ReadLine()) != null) // 1行ずつ読み出し。
+                {
+                    richTextBox1.AppendText( line ) ;
+                }
+            }
+        }
+
+        private void PlotSinCos()
+        {
+            // 1.Seriesの追加
+            chart1.Series.Clear();
+            chart1.Series.Add("sin");
+            chart1.Series.Add("cos");
+
+            // 2.グラフのタイプの設定
+            chart1.Series["sin"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series["cos"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
+            // 3.座標の入力
+            for (double theta = 0.0; theta <= 2 * Math.PI; theta += Math.PI / 360)
+            {
+                //chart1.Series["sin"].Points.AddXY(theta, Math.Sin(theta));
+                chart1.Series["cos"].Points.AddXY(theta, Math.Cos(theta));
+            }
+
+            for (int i = 0; i < obs_data_max; i++)
+            {
+                chart1.Series["sin"].Points.AddXY(obs_data[i].X, obs_data[i].Y);
+            }
+
+            //軸ラベルの設定
+            string s = chart1.Series["sin"].ChartArea;//
+            chart1.ChartAreas[s].AxisX.Title = "WaveLenght(nm)";
+            chart1.ChartAreas[s].AxisY.Title = "Relative Value";
+            //※Axis.TitleFontでフォントも指定できるがこれはデザイナで変更したほうが楽
+
+            //X軸最小値、最大値、目盛間隔の設定
+            chart1.ChartAreas[s].AxisX.Minimum = 300;
+            chart1.ChartAreas[s].AxisX.Maximum = 1200;
+            chart1.ChartAreas[s].AxisX.Interval = 50;
+
+            //Y軸最小値、最大値、目盛間隔の設定
+            //chart1.ChartAreas["area1"].AxisY.Minimum = -1;
+            //chart1.ChartAreas["area1"].AxisY.Maximum = 1;
+            //chart1.ChartAreas["area1"].AxisY.Interval = 0.2;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ReadData();
+
+            PlotSinCos();
         }
     }
 }
